236e163d8411a9d5d875b6753f4b95483507b678
Documenter.jl <documenter@juliadocs.github.io>
2024-03-02 15:33:58 +0000

    build based on f5952ff

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExcitonsBSE","category":"page"},{"location":"#ExcitonsBSE","page":"Home","title":"ExcitonsBSE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ExcitonsBSE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExcitonsBSE]","category":"page"}]
+}
